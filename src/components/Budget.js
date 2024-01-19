54eb38adf5d75f7263a0164ddfbfec734bcb6a93
Debora <debo4978@gamil.com>
2024-01-18 23:20:47 -0300

    creando componentes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,3 +5,20 @@ Se importa el context de la app y  useContext hook y pasa la AppContext
 (asi un componente se conecta al context 
 para obtener valores del estado global) 
  */
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
