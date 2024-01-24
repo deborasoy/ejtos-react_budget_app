779b9b2414eb13924e16125e1f7d2105f63099b5
Debora <debo4978@gamil.com>
2024-01-24 15:31:12 -0300

    comentarios en el codigo

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,17 +1,17 @@
-/*
-importa (gatos y presupuestos) desde el context y obtiene el valor restante
-*/
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
+    const { expenses, budget, currency } = useContext(AppContext);
+    /*calcula el total de presupuesto ya invertido en los departamento, sumando cada presupuesto asignado por departamento */
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
+    /*en caso del presupuesto invertido sea inferior al presupuesto del capital de la empresa, muestra el background rojo, sino gris */
+    const alertType = totalExpenses >= budget ? 'alert-danger' : 'alert-dark';
+    return ( /*templeate con el letrero del presupuesto restante disponible para invertir en los departamentos */
+        <div className={`alert ${alertType} text-center`}>
+            <span style={{ fontSize:"20px"}}>Remaining:{currency} {budget - totalExpenses}</span>
         </div>
     );
 };
