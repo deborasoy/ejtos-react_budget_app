76d266e0260fb9fa5fc1959941d378e263f8a6b6
Debora <debo4978@gamil.com>
2024-01-24 15:28:11 -0300

    funcionalidades, diseño, boton reduccion

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,34 +6,46 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
-
+    const { dispatch, currency } = useContext(AppContext);
+    /*resetear el valor del presupuesto asignado al departamento a 0 por su id (funcionalidad del boton "x") y modifca en ambito global */
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
             payload: props.id,
         });
     };
-
+    /*incrementar el presupuesto de a 10 por departamento (funcionalidad del boton "+")  */
     const increaseAllocation = (name) => {
         const expense = {
             name: name,
             cost: 10,
         };
-
+        /*modifica en el ambito global appContext el aumento del presupuesto con el boton (+) */
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
-
+    }
+    /*reduce el presupuesto de a 10 por departamento (funcionalidad del boton "-") */
+    const reduceAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        /*modifica en el ambito global appContext, reduce el  presupuesto con el boton (-) */
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
     }
 
-    return (
+    return ( /*templeate con el valor del body de la tabla creada en componente ExpenseList */
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td style={{ fontSize:"24px"}}>{props.name}</td>
+        <td style={{ fontSize:"24px"}}>{currency}{props.cost}</td>
+        <td><button className='btn btn-success' onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className='btn btn-danger' onClick={event=> reduceAllocation(props.name)}>-</button></td>
+        <td style={{ cursor:"pointer"}}><TiDelete size='2em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
