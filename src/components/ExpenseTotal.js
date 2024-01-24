ff5500495d560d8fb85d4844775331f43588ae24
Debora <debo4978@gamil.com>
2024-01-24 15:30:17 -0300

    cambios en la moneda, comentarios en el codigo

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,18 +1,16 @@
-/*
-agrega UseContext y AppContext
-Toma los gatos del estado
-*/
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
+    const { expenses, currency } = useContext(AppContext);
+    /*calucla el gasto total o el total de presupuesto invertido, sumando los cotos o presupuestos asignados por departamento */
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
-    return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
-        </div>
+    return (/*templeate con el letrero de spent, el total de presupuesto ya invertido en los departamentos */
+        < div className='alert alert-dark text-center'>
+             <span style={{ fontSize:"20px"}}>Spent so far: {currency} {totalExpenses}</span>
+         </div>    
     );
 };
-export default ExpenseTotal;
+export default ExpenseTotal
