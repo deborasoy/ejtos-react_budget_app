47db6f3cc017a9096a4bd6f0ee428b5af58c0d85
Debora <debo4978@gamil.com>
2024-01-24 15:29:07 -0300

    comentarios en el codigo, agrego columna tabla

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,4 @@
-/*
-usa la funcion map para mostrar el componente: Expenseitem
-*/
+
 import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
@@ -8,13 +6,14 @@ import { AppContext } from '../context/AppContext';
 const ExpenseList = () => {
     const { expenses } = useContext(AppContext);
 
-    return (
+    return ( /*templeate con la tabla con info de los departamento, se asinga la data del body desde el componente ExpenseItem */
         <table className='table'>
               <thead className="thead-light">
             <tr>
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Reduce by 10</th>
               <th scope="col">Delete</th>
             </tr>
           </thead>
